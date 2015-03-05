d243f011bafd4c9cde9c4639a230363bb1ee0fd7
Ivar Conradi Østhus <ivarconr@gmail.com>
2015-03-05 22:13:21 +0100

    Added travis. #3

--- examples/simple_usage.js
+++ examples/simple_usage.js
@@ -1,20 +1,6 @@
 var unleash = require('./unleash');
-var util = require('util');
-
-function ActiveForUserWithEmailStrategy() {
-    this.name = 'ActiveForUserWithEmail';
-}
-
-util.inherits(ActiveForUserWithEmailStrategy, unleash.Strategy);
-
-ActiveForUserWithEmailStrategy.prototype.isEnabled = function(parameters, context) {
-    return parameters.emails.indexOf(context.email) !== -1;
-};
-
 unleash.initialize({
-    url: 'http://unleash.herokuapp.com/features',
-    refreshIntervall: 10000,
-    strategies: [new ActiveForUserWithEmailStrategy()]
+    url: 'http://unleash.herokuapp.com/features'
 });
 
 console("Fetching toggles from: http://unleash.herokuapp.com");
