d736772adb04da27848af1aec7ca1e4f9e735c45
sveisvei <sveinung.rosaker@gmail.com>
2016-11-05 20:31:44 +0100

    wip

--- src/strategy.ts
+++ src/strategy.ts
@@ -1,8 +1,13 @@
 'use strict';
 
+export interface StrategyTransportInterface {
+    name: string;
+    parameters: any;
+};
+
 export class Strategy {
     public name: string;
-    private returnValue: boolean;
+    public returnValue: boolean;
 
     constructor (name: string, returnValue: boolean = false) {
         this.name = name || 'unknown';
@@ -14,7 +19,68 @@ export class Strategy {
     }
 };
 
-export interface StrategyTransportInterface {
-    name: string;
-    parameters: any;
+// export function Simple(parameters: any, context: any) : boolean {
+//     return true;
+// };
+// Simple.prototype.name = 'simple-matcher';
+
+
+export class CookieMatcher extends Strategy {
+    constructor () {
+        super('cookie-matcher');
+    }
+
+    isEnabled (parameters: any, context: any) : boolean {
+        return this.returnValue;
+    }
+};
+
+interface User {
+    id: string,
+};
+
+export class UserMatcher extends Strategy {
+    constructor () {
+        super('user-matcher');
+    }
+
+    isEnabled (parameters: User, context: User) : boolean {
+        for (var key in parameters) {
+            if (parameters.hasOwnProperty(key)) {
+                if (parameters[key] === ) {
+
+                }
+            }
+        }
+    }
+};
+
+export class GradualRolloutRandom extends Strategy {
+    constructor () {
+        super('gradual-rollout-random');
+    }
+
+    isEnabled (parameters: any, context: any) : boolean {
+        return this.returnValue;
+    }
+};
+
+export class ByHostName extends Strategy {
+    constructor () {
+        super('by-host-name');
+    }
+
+    isEnabled (parameters: any, context: any) : boolean {
+        return this.returnValue;
+    }
+};
+
+export class ByRemoteAddr extends Strategy {
+    constructor () {
+        super('by-remote-addr');
+    }
+
+    isEnabled (parameters: any, context: any) : boolean {
+        return this.returnValue;
+    }
 };
