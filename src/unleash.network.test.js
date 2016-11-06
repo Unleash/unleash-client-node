6aa38b5d6a09617cce735b42db28318a4aa320a0
sveisvei <sveinung.rosaker@gmail.com>
2016-11-06 10:26:07 +0100

    add unleash network test

---
+++ src/unleash.network.test.js
@@ -0,0 +1,35 @@
+import test from 'ava';
+import nock from 'nock';
+import { tmpdir } from 'os';
+import { join } from 'path';
+import { Unleash } from '../lib/unleash';
+
+test.before(() => nock.disableNetConnect());
+test.after(() => nock.enableNetConnect());
+
+test.cb('should emit network errors', (t) => {
+    t.plan(3);
+    const backupPath = join(tmpdir(), `test-tmp-${Math.round(Math.random() * 100000)}`);
+    const unleash = new Unleash({
+        appName: 'network',
+        url: 'http://blocked.app',
+        refreshInterval: 20000,
+        metricsInterval: 20000,
+        disableMetrics: false,
+        backupPath,
+    });
+
+    unleash.on('error', (e) => {
+        t.truthy(e);
+    });
+
+    unleash.isEnabled('some-toggle');
+    unleash.metrics.sendMetrics();
+
+    setTimeout(() => {
+        unleash.destroy();
+        process.nextTick(() => {
+            t.end();
+        });
+    }, 0);
+});
