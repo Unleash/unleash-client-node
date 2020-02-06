59a93d0d9fe4469cd4517441a02ba4a444300269
Ivar Conradi Østhus <ivarconr@gmail.com>
2020-02-06 22:04:01 +0100

    fix: clean up properties types a bit

--- src/context.ts
+++ src/context.ts
@@ -1,9 +1,13 @@
-export interface Context {
+export interface Propeties {
     [key: string]: string | undefined | number;
+}
+
+export interface Context {
+    [key: string]: string | undefined | number | Propeties;
     userId?: string;
     sessionId?: string;
     remoteAddress?: string;
     environment?: string;
     appName?: string;
-    properties?: any;
+    properties?: Propeties;
 }
