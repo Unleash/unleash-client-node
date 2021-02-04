de4cf1167d7f6c9a0debf4ae8cfdcf7bba1a5550
Ivar Conradi Østhus <ivarconr@gmail.com>
2021-02-04 13:43:54 +0100

    chore: add a small test script to trigger all toggles

---
+++ examples/test_all_toggles.js
@@ -0,0 +1,22 @@
+const { initialize, isEnabled, getFeatureToggleDefinitions } = require('../lib');
+
+const client = initialize({
+    appName: 'test-all-toggles',
+    url: 'https://unleash.herokuapp.com/api',
+    metricsInterval: 1000,
+    refreshInterval: 5000,
+    customHeaders: {
+        Authorization: '56907a2fa53c1d16101d509a10b78e36190b0f918d9f122d',
+    },
+});
+
+client.on('error', console.error);
+client.on('warn', console.log);
+
+console.log('Fetching toggles from: https://unleash.herokuapp.com');
+
+setInterval(() => {
+    getFeatureToggleDefinitions().forEach(t => {
+        console.log(`${t.name}: ${isEnabled(t.name)}`);
+    });
+}, 1000);
