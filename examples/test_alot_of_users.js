c3e990d3c57997dd596d223986263a310bce5b7d
Ivar Conradi Østhus <ivarconr@gmail.com>
2021-02-12 12:31:28 +0100

    fix: add another test-script

---
+++ examples/test_alot_of_users.js
@@ -0,0 +1,25 @@
+const { initialize } = require('../lib');
+
+const client = initialize({
+    appName: 'test-all-toggles',
+    url: 'https://unleash.herokuapp.com/api',
+    metricsInterval: 1000,
+    refreshInterval: 5000,
+    customHeaders: {
+        Authorization: '56907a2fa53c1d16101d509a10b78e36190b0f918d9f122d',
+    },
+});
+
+client.on('error', console.error);
+client.on('warn', console.log);
+client.on('unchanged', () => console.error('NOT CHANGED'));
+client.on('changed', () => console.log('changed'));
+
+console.log('Fetching toggles from: https://unleash.herokuapp.com');
+
+const toggle = 'Demo';
+
+setInterval(() => {
+    const enabled = client.isEnabled(toggle, { userId: `${Math.random() * 10000}` });
+    console.log(`${toggle}: ${enabled}`);
+}, 100);
