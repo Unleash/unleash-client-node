d4b90386a3376172813688046657dd80112a4450
Ivar Conradi Østhus <ivarconr@gmail.com>
2021-02-19 12:39:27 +0100

    revert examples/simple_usage

--- examples/simple_usage.js
+++ examples/simple_usage.js
@@ -2,9 +2,7 @@ const { initialize, isEnabled } = require('../lib');
 
 const client = initialize({
   appName: 'my-application',
-  url: 'http://unleash2.herokuapp.com/api/',
-  refreshInterval: 1,
-  metricsInterval: 100,
+  url: 'http://unleash.herokuapp.com/api/',
 });
 
 client.on('error', console.error);
@@ -14,4 +12,4 @@ console.log('Fetching toggles from: http://unleash.herokuapp.com');
 
 setInterval(() => {
   console.log(`featureX enabled: ${isEnabled('featureX')}`);
-}, 100);
+}, 1000);
