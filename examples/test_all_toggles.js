c03ebbbe2e7cb12e137365a76207c6ba8c843736
Christopher Kolstad <chriswk@getunleash.ai>
2021-10-20 12:11:51 +0200

    Make test_all_toggles accept environment variables for url and api key

--- examples/test_all_toggles.js
+++ examples/test_all_toggles.js
@@ -1,12 +1,14 @@
 const { initialize, isEnabled, getFeatureToggleDefinitions } = require('../lib');
 
+const url = process.env.UNLEASH_API_URL || 'https://app.unleash-hosted.com/demo/api';
+const apiKey = process.env.UNLEASH_API_KEY || '56907a2fa53c1d16101d509a10b78e36190b0f918d9f122d';
 const client = initialize({
   appName: 'test-all-toggles',
-  url: 'https://app.unleash-hosted.com/demo/api',
+  url,
   metricsInterval: 1000,
   refreshInterval: 5000,
   customHeaders: {
-    Authorization: '56907a2fa53c1d16101d509a10b78e36190b0f918d9f122d',
+    Authorization: apiKey,
   },
 });
 
@@ -15,7 +17,7 @@ client.on('warn', console.log);
 client.on('unchanged', () => console.error('NOT CHANGED'));
 client.on('changed', () => console.log('changed'));
 
-console.log('Fetching toggles from: https://unleash.herokuapp.com');
+console.log(`Fetching toggles from: ${url}`);
 
 setInterval(() => {
   getFeatureToggleDefinitions().forEach((t) => {
