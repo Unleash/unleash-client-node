099093b6a74ebeab3f61fb1fff7cbfcd58bcb2ce
kwasniew <kwasniewski.mateusz@gmail.com>
2024-12-05 10:47:40 +0100

    chore: streaming example

--- examples/variant.js
+++ examples/variant.js
@@ -1,14 +1,18 @@
-const { Unleash } = require('../lib');
+const { initialize, getVariant } = require('../lib');
 
-const client = new Unleash({
+const client = initialize({
   appName: 'my-application',
-  url: 'https://sandbox.getunleash.io/nuno/api/',
+  url: 'https://unleash.herokuapp.com/api/',
   customHeaders: {
-    Authorization: '',
+    Authorization: '3bd74da5b341d868443134377ba5d802ea1e6fa2d2a948276ade1f092bec8d92',
   },
-  experimentalMode: { type: 'streaming' },
-  skipInstanceCountWarning: true,
 });
-client.on('changed', () => {
-  console.log('Enabled:', client.isEnabled('streaming-flag', { userId: `${Math.random()}` }));
-});
+
+client.on('error', console.error);
+client.on('warn', console.log);
+
+console.log('Fetching toggles from: http://unleash.herokuapp.com');
+
+setInterval(() => {
+  console.log('Variant config', getVariant('Test.variants', { userId: `${Math.random()}` }));
+}, 1000);
