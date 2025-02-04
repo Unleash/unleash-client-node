cfaa46919c61774a01a1ea958db3cd7b84e4a56f
Tymoteusz Czech <2625371+Tymek@users.noreply.github.com>
2025-02-04 14:23:47 +0100

    refactor: comment on event-source

--- src/event-source.js
+++ src/event-source.js
@@ -1,3 +1,3 @@
 import { EventSource as moduleToPatch } from 'launchdarkly-eventsource';
 
-export const EventSource = moduleToPatch;
+export const EventSource = moduleToPatch; // Re-export from .js file, because the original module doesn't have types
