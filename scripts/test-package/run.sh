cdc379e08526ac086c0775b7d32e409fd0b000d1
Tymoteusz Czech <2625371+Tymek@users.noreply.github.com>
2025-02-04 13:52:11 +0100

    test: add install testing script to workflow

--- scripts/test-package/run.sh
+++ scripts/test-package/run.sh
@@ -2,8 +2,8 @@
 set -e
 
 TEST_DIR="test-package"
-
 mkdir "$TEST_DIR"
+
 cp scripts/test-package/test-tsconfig.json "$TEST_DIR/tsconfig.json"
 cp scripts/test-package/test-package.json "$TEST_DIR/package.json"
 cd "$TEST_DIR"
@@ -11,5 +11,13 @@ npm install --install-links
 mkdir src
 echo -e "import { Unleash } from 'unleash-client';\nvoid Unleash;\nconsole.log('Hello world');" > src/index.ts
 ./node_modules/.bin/tsc -b tsconfig.json
-cd ..
-rm -rf "$TEST_DIR"
+
+if [ "$(node . 2>&1)" = "Hello world" ]; then
+  echo "Output is correct"
+  (cd .. && rm -rf "$TEST_DIR")
+else
+  echo "Output is incorrect" >&2
+  echo $(node . 2>&1)
+  (cd .. && rm -rf "$TEST_DIR")
+  exit 1
+fi
