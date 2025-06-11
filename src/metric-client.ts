d12d24a186f88927bd9af3da62949b15b0c63c49
Simon Hornby <liquidwicked64@gmail.com>
2025-06-11 15:20:58 +0200

    chore: add explicit methods to decorator

--- src/metric-client.ts
+++ src/metric-client.ts
@@ -1,10 +1,6 @@
 import { Unleash } from './unleash';
 
-function forwardMethods<T extends object>(
-  target: UnleashMetricClient,
-  source: Unleash,
-  methodNames: (keyof T)[],
-) {
+function forwardMethods<T extends object>(target: T, source: T, methodNames: (keyof T)[]) {
   for (const name of methodNames) {
     // @ts-ignore
     target[name] = (...args: any[]) => source[name](...args);
@@ -16,7 +12,7 @@ export class UnleashMetricClient {
 
   constructor(unleash: Unleash) {
     this.backingInstance = unleash;
-    forwardMethods(this, this.backingInstance, [
+    forwardMethods(this, this.backingInstance as any, [
       'isEnabled',
       'getVariant',
       'forceGetVariant',
@@ -32,4 +28,30 @@ export class UnleashMetricClient {
       'isSynchronized',
     ]);
   }
+
+  isEnabled!: Unleash['isEnabled'];
+
+  getVariant!: Unleash['getVariant'];
+
+  forceGetVariant!: Unleash['forceGetVariant'];
+
+  getFeatureToggleDefinition!: Unleash['getFeatureToggleDefinition'];
+
+  getFeatureToggleDefinitions!: Unleash['getFeatureToggleDefinitions'];
+
+  count!: Unleash['count'];
+
+  countVariant!: Unleash['countVariant'];
+
+  flushMetrics!: Unleash['flushMetrics'];
+
+  destroyWithFlush!: Unleash['destroyWithFlush'];
+
+  on!: Unleash['on'];
+
+  start!: Unleash['start'];
+
+  destroy!: Unleash['destroy'];
+
+  isSynchronized!: Unleash['isSynchronized'];
 }
