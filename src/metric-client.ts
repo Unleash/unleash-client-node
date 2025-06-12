e6d4d3471c786ce64996bb39d8713d14e7bdcde7
Simon Hornby <liquidwicked64@gmail.com>
2025-06-12 15:00:26 +0200

    chore: add a proxy decorator to the client so we can add impact metrics unobtrusively (#738)

---
+++ src/metric-client.ts
@@ -0,0 +1,57 @@
+import { Unleash } from './unleash';
+
+function forwardMethods<T extends object>(target: T, source: T, methodNames: (keyof T)[]) {
+  for (const name of methodNames) {
+    // @ts-ignore
+    target[name] = (...args: any[]) => source[name](...args);
+  }
+}
+
+export class UnleashMetricClient {
+  private backingInstance: Unleash;
+
+  constructor(unleash: Unleash) {
+    this.backingInstance = unleash;
+    forwardMethods(this, this.backingInstance as any, [
+      'isEnabled',
+      'getVariant',
+      'forceGetVariant',
+      'getFeatureToggleDefinition',
+      'getFeatureToggleDefinitions',
+      'count',
+      'countVariant',
+      'flushMetrics',
+      'destroyWithFlush',
+      'on',
+      'start',
+      'destroy',
+      'isSynchronized',
+    ]);
+  }
+
+  isEnabled!: Unleash['isEnabled'];
+
+  getVariant!: Unleash['getVariant'];
+
+  forceGetVariant!: Unleash['forceGetVariant'];
+
+  getFeatureToggleDefinition!: Unleash['getFeatureToggleDefinition'];
+
+  getFeatureToggleDefinitions!: Unleash['getFeatureToggleDefinitions'];
+
+  count!: Unleash['count'];
+
+  countVariant!: Unleash['countVariant'];
+
+  flushMetrics!: Unleash['flushMetrics'];
+
+  destroyWithFlush!: Unleash['destroyWithFlush'];
+
+  on!: Unleash['on'];
+
+  start!: Unleash['start'];
+
+  destroy!: Unleash['destroy'];
+
+  isSynchronized!: Unleash['isSynchronized'];
+}
