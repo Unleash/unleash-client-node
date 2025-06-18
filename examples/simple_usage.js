970435e7305d5ae7b1217f57372e7a69e69f9333
Simon Hornby <liquidwicked64@gmail.com>
2025-06-18 16:25:10 +0200

    chore: restore example

--- examples/simple_usage.js
+++ examples/simple_usage.js
@@ -1,15 +1,14 @@
-const { UnleashMetricClient } = require('../lib');
+const { initialize, isEnabled } = require('../lib');
 
-const url = 'http://localhost:4242/api/';
-const apiToken = '*:development.a80d09556cf83ef8f53b031d2bd941a3646da384c3976a4cd9991cc5';
-const toggleName = 'testflag';
+const url = 'https://app.unleash-hosted.com/demo/api/';
+const apiToken = '943ca9171e2c884c545c5d82417a655fb77cec970cc3b78a8ff87f4406b495d0';
+const toggleName = 'demo001';
 const unleashContext = { userId: '1232' };
 
-const unleash = new UnleashMetricClient({
+const unleash = initialize({
   appName: 'my-application',
   url,
   refreshInterval: 1000,
-  metricsInterval: 1000,
   customHeaders: {
     Authorization: apiToken,
   },
@@ -21,12 +20,9 @@ unleash.on('ready', () => {
   console.log('ready!');
 });
 
-unleash.impactMetrics.defineCounter('test', 'some-help-text');
-
 console.log(`Fetching toggles from: ${url}`);
 
 setInterval(() => {
-  unleash.impactMetrics.incrementCounter('test');
-  const enabled = unleash.isEnabled(toggleName, unleashContext);
+  const enabled = isEnabled(toggleName, unleashContext);
   console.log(`Enabled: ${enabled}`);
 }, 1000);
