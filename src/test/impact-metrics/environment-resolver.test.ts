048f90f6546fd556d2c707f3bee14f5f748b45e8
kwasniew <kwasniewski.mateusz@gmail.com>
2025-07-10 12:57:48 +0200

    feat: impact metrics env resolver

---
+++ src/test/impact-metrics/environment-resolver.test.ts
@@ -0,0 +1,36 @@
+import test from 'ava';
+import { extractEnvironmentFromCustomHeaders } from '../../impact-metrics/environment-resolver';
+
+test('valid headers', (t) => {
+  const customHeaders = {
+    Authorization: 'project:environment:hash',
+    'Content-Type': 'application/json',
+  };
+
+  const result = extractEnvironmentFromCustomHeaders(customHeaders);
+  t.is(result, 'environment');
+});
+
+test('case-insensitive header keys', (t) => {
+  const customHeaders = {
+    AUTHORIZATION: 'project:environment:hash',
+    'Content-Type': 'application/json',
+  };
+
+  const result = extractEnvironmentFromCustomHeaders(customHeaders);
+  t.is(result, 'environment');
+});
+
+test('authorization header not present', (t) => {
+  const result = extractEnvironmentFromCustomHeaders({});
+  t.is(result, undefined);
+});
+
+test('environment part is empty', (t) => {
+  const customHeaders = {
+    Authorization: 'project::hash',
+  };
+
+  const result = extractEnvironmentFromCustomHeaders(customHeaders);
+  t.is(result, undefined);
+});
